3f16a708c59413c2caf4ca63bed470fc49b9c20a
Emile Belanger <emile.belanger@gmail.com>
2024-12-20 16:37:09 +0000

    Re-enable code optimisation

--- mobile/Android.mk
+++ mobile/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := fteqw_dev
 
-LOCAL_CFLAGS := -O0 -g -DGLQUAKE -DLIBVORBISFILE_STATIC -DLIBJPEG_STATIC -DLIBJPEG_STATIC -DLIBPNG_STATIC  -DFREETYPE_STATIC  -Wno-write-strings -DFTEQW -DENGINE_NAME=\"fteqw\"
+LOCAL_CFLAGS :=  -DGLQUAKE -DLIBVORBISFILE_STATIC -DLIBJPEG_STATIC -DLIBJPEG_STATIC -DLIBPNG_STATIC  -DFREETYPE_STATIC  -Wno-write-strings -DFTEQW -DENGINE_NAME=\"fteqw\"
 LOCAL_CFLAGS += -Wall -Wno-pointer-sign -Wno-unknown-pragmas -Wno-format-zero-length -Wno-strict-aliasing
 LOCAL_CFLAGS += -Dstrnicmp=strncasecmp -Dstricmp=strcasecmp -fsigned-char
 
